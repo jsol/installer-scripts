d805411336a46d27fd9bbbd9fc34630eb68e062e
Jens Olsson <jens@rootsy.nu>
2024-12-06 00:45:21 +0100

    Fix json meta download when not logged in

--- helpers.sh
+++ helpers.sh
@@ -11,8 +11,8 @@ github_ensure_file(){
       curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" \
         "https://api.github.com/repos/$user/$repo/releases/latest" > "$WORKDIR/meta.${user}.${repo}.json"
     else
-      curl -vi -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/$user/$repo/releases/latest"
-    fi 
+      curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/$user/$repo/releases/latest" > "$WORKDIR/meta.${user}.${repo}.json"
+    fi
   fi
 
   if ! jq -e . >/dev/null "$WORKDIR/meta.${user}.${repo}.json"; then
